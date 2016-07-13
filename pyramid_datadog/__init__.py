7d1fd1999be54116a0dfa7d5ece65d16c41237d4
Paola Castro <paolacastropaolacastro@gmail.com>
2016-07-13 23:39:40 +0000

    draft on_new_request / on_new_response subscribers

--- pyramid_datadog/__init__.py
+++ pyramid_datadog/__init__.py
@@ -1,4 +1,4 @@
-from pyramid.events import ApplicationCreated
+from pyramid.events import ApplicationCreated, NewResponse, NewRequest
 
 
 def configure_metrics(config, datadog_metrics):
@@ -15,6 +15,17 @@ def on_app_created(app_created_event):
     )
 
 
+def on_new_request(new_request_event):
+    print 'Pyramid triggered a NewRequest event'
+    datadog = new_request_event.request.registry.datadog
+    datadog.increment('pyramid.request.count', tags=['pyramid_datadog'])
+
+def on_new_response(new_response_event):
+    print 'Pyramid triggered a NewResponse event'
+
+
 def includeme(config):
     config.add_directive('configure_metrics', configure_metrics)
     config.add_subscriber(on_app_created, ApplicationCreated)
+    config.add_subscriber(on_new_request, NewRequest)
+    config.add_subscriber(on_new_response, NewResponse)
