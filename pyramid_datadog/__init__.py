c56ee8f182bec4824b45bfb74d2717bf6acc67cd
Paola Castro <paolac@surveymonkey.com>
2016-07-25 13:25:51 -0700

    use namespace in metric and some reformatting

--- pyramid_datadog/__init__.py
+++ pyramid_datadog/__init__.py
@@ -14,8 +14,9 @@ def time_ms():
     return time.time() * 1000
 
 
-def configure_metrics(config, datadog_metrics):
+def configure_metrics(config, datadog_metrics, namespace=None):
     config.registry.datadog = datadog_metrics
+    config.registry.datadog_namespace = namespace
 
 
 def on_app_created(app_created_event):
@@ -33,7 +34,7 @@ def on_new_request(new_request_event):
     request.timings['new_request_start'] = time_ms()
     datadog = request.registry.datadog
     datadog.increment(
-        'pyramid.request.count',
+        '%s.request.count' % request.registry.datadog_namespace,
         tags=['pyramid_datadog'],
     )
 
@@ -41,10 +42,11 @@ def on_new_request(new_request_event):
 def on_before_traversal(before_traversal_event):
     request = before_traversal_event.request
     timings = request.timings
-    datadog = request.registry.datadog
     timings['route_match'] = time_ms() - timings['new_request_start']
+
+    datadog = request.registry.datadog
     datadog.timing(
-        'pyramid.route_match.duration',
+        '%s.route_match.duration' % request.registry.datadog_namespace,
         timings['route_match_duration'],
         tags=['pyramid_datadog'],
     )
@@ -52,12 +54,14 @@ def on_before_traversal(before_traversal_event):
 
 def on_context_found(context_found_event):
     request = context_found_event.request
+
     timings = request.timings
     timings['traversal_duration'] = time_ms() - timings['new_request_start']
     timings['view_code_start'] = time_ms()
+
     datadog = request.registry.datadog
     datadog.timing(
-        'pyramid.traversal.duration',
+        '%s.traversal.duration' % request.registry.datadog_namespace,
         timings['traversal_duration'],
         tags=['pyramid_datadog'],
     )
@@ -65,45 +69,55 @@ def on_context_found(context_found_event):
 
 def on_before_render(before_render_event):
     request = before_render_event['request']
+
     timings = request.timings
     timings['view_duration'] = time_ms() - timings['view_code_start']
+    timings['before_render_start'] = time_ms()
+
     datadog = request.registry.datadog
     datadog.timing(
-        'pyramid.view_duration',
+        '%s.view_duration' % request.registry.datadog_namespace,
         timings['view_duration'],
         tags=['pyramid_datadog'],
     )
-    timings['before_render_start'] = time_ms()
 
 
 def on_new_response(new_response_event):
     request = new_response_event.request
-    timings = request.timings
     new_response_time = time_ms()
+
+    timings = request.timings
     timings['request_duration'] = \
         new_response_time - timings['new_request_start']
     timings['template_render_duration'] = \
         new_response_time - timings['before_render_start']
+
     datadog = request.registry.datadog
     datadog.timing(
-        'pyramid.request.duration',
+        '%s.request.duration' % request.registry.datadog_namespace,
         timings['request_duration'],
         tags=['pyramid_datadog'],
     )
     datadog.timing(
-        'pyramid.template_render.duration',
+        '%s.template_render.duration' % request.registry.datadog_namespace,
         timings['template_render_duration'],
         tags=['pyramid_datadog'],
     )
 
     status_code = request.response.status
     datadog.increment(
-        'pyramid.response.http.status_code.%s' % status_code,
+        '%s.response.http.status_code.%s' % (
+            request.registry.datadog_namespace,
+            status_code
+        ),
         tags=['pyramid_datadog'],
     )
 
     datadog.increment(
-        'pyramid.response.http.status_code.%sxx' % status_code[0],
+        '%s.response.http.status_code.%sxx' % (
+            request.registry.datadog_namespace,
+            status_code[0]
+        ),
         tags=['pyramid_datadog'],
     )
 
